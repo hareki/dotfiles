8880e1aad4e03d2651331f46b0c07fdc4f1788d9
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-10 22:06:34 +0700

    updated via script at 10/08/2024 | Sat 10:06 PM

--- nvim/.config/nvim/lua/plugins/extras/ui.lua
+++ nvim/.config/nvim/lua/plugins/extras/ui.lua
@@ -1,38 +1,22 @@
-function Get_initial_path()
-  -- Get the first argument passed to Neovim (which is usually the path)
-  local first_arg = vim.fn.argv(0)
-
-  -- If the path is relative, resolve it to an absolute path
-  local initial_path = vim.fn.fnamemodify(first_arg, ":p")
-
-  return initial_path
-end
 return {
   {
     "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
   },
   {
     "Bekaboo/dropbar.nvim",
+    lazy = false,
     -- optional, but required for fuzzy finder support
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
     },
+    keys = {
+      { "<leader>d", "<cmd>lua require('dropbar.api').pick()<cr>", desc = "Dropbar pick" },
+    },
     opts = {
       sources = {
         path = {
-          relative_to = function(_, win)
-            -- return "/home/hareki/.config/nvim"
-            return Get_initial_path()
-            -- Workaround for Vim:E5002: Cannot find window number
-            -- local ok, cwd = pcall(vim.fn.getcwd, win)
-            -- return ok and cwd or vim.fn.getcwd()
-
-            -- return "/home/hareki/Repositories/personal/dotfiles/"
-            -- return LazyVim.lualine.pretty_path({
-            --   relative = "cwd",
-            --   length = 0,
-            --   filename_hl = "none",
-            -- })
+          relative_to = function()
+            return Util.get_initial_path()
           end,
         },
       },
