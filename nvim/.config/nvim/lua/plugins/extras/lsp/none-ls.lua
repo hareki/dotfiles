c2d9a0454d5aa9977814e2039e737f18a56acbbf
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-20 07:53:48 +0700

    updated via script at 20/08/2024 | Tue 07:53 AM

--- nvim/.config/nvim/lua/plugins/extras/lsp/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/extras/lsp/none-ls.lua
@@ -1,3 +1,10 @@
+local null_ls = require("null-ls")
+
+local config = {
+  find_json = function()
+    return vim.fn.expand("~") .. '/.config/cspell.json'
+  end,
+}
 return {
   "nvimtools/none-ls.nvim",
   dependencies = {
@@ -5,7 +12,7 @@ return {
   },
   config = function()
     local cspell = require("cspell")
-    require("null-ls").setup({
+    null_ls.setup({
       sources = {
         cspell.diagnostics.with({
           -- https://www.reddit.com/r/neovim/comments/zyv7pi/nullls_warning_level_for_sources/
@@ -13,9 +20,29 @@ return {
           diagnostics_postprocess = function(diagnostic)
             diagnostic.severity = vim.diagnostic.severity.INFO
           end,
+          config = config
         }),
-        cspell.code_actions,
+        cspell.code_actions.with({
+          config = config
+        }),
       },
     })
   end,
+
+  keys = {
+    {
+      "<leader>uS",
+      function()
+        local cspell_query = { name = "cspell" }
+        local cspell_active = not null_ls.get_source(cspell_query)[1]._disabled
+
+        local next_cspell_active = not cspell_active
+        local notify = next_cspell_active and LazyVim.info or LazyVim.warn
+        notify(Util.toggle_notify.run("cspell", next_cspell_active), { title = "cspell" })
+
+        null_ls.toggle(cspell_query)
+      end,
+      desc = "Toggle cspell",
+    },
+  },
 }
