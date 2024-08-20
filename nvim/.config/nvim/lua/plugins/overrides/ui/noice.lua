be4d1e93cb5ca6ada1f1825e9c04908aacd0a401
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-20 07:53:48 +0700

    updated via script at 20/08/2024 | Tue 07:53 AM

--- nvim/.config/nvim/lua/plugins/overrides/ui/noice.lua
+++ nvim/.config/nvim/lua/plugins/overrides/ui/noice.lua
@@ -17,5 +17,33 @@ return {
         },
       },
     },
+    routes = {
+      {
+        -- Ignore null-ls messages since it's just cspell
+        filter = {
+          event = "lsp",
+          kind = "progress",
+          cond = function(message)
+            local client = vim.tbl_get(message.opts, "progress", "client")
+            return client == "null-ls"
+          end,
+        },
+        opts = { skip = true },
+      },
+
+      -- Add back this option since lazy.nvim doesn't perform a deep merge (it shouldn't anyway)
+      -- https://www.lazyvim.org/plugins/ui#noicenvim
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+    },
   },
 }
