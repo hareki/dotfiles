e50e2fe6abf466d94c3e65673c6ec377c89bff7c
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-23 19:34:21 +0700

    update clipboard config

--- nvim/.config/nvim/lua/plugins/extras/coding/yanky.nvim.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/yanky.nvim.lua
@@ -1,3 +1,4 @@
+-- NOTE: disabled
 return {
   "gbprod/yanky.nvim",
   dependencies = {
@@ -6,15 +7,19 @@ return {
   opts = {
     ring = { storage = "sqlite" },
     highlight = {
+      on_yank = false,
       on_put = true,
-      on_yank = true,
-      timer = 150,
+      timer = Constant.YANK_PUT_HL_TIMER,
+    },
+    system_clipboard = {
+      sync_with_ring = false,
+      clipboard_register = nil,
     },
   },
   keys = {
     { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({}) end, desc = "Open Yank History" },
     { "y",         "<Plug>(YankyYank)",                                                          mode = { "n", "x" },                                desc = "Yank text" },
-    { "p",         "<Plug>(YankyPutAfter)",                                                      mode = { "n", "x" },                                desc = "Put yanked text after cursor" },
+    { "p",         '<Plug>(YankyPutAfter)',                                                      mode = { "n", "x" },                                desc = "Put yanked text after cursor without overwriting register" },
     { "<c-p>",     "<Plug>(YankyPreviousEntry)",                                                 desc = "Select previous entry through yank history" },
     { "<c-n>",     "<Plug>(YankyNextEntry)",                                                     desc = "Select next entry through yank history" },
   },
