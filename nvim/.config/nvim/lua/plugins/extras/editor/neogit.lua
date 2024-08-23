56d5abc62b11b840b507f968769834dbe251f02f
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-23 10:15:27 +0700

    this commit is from neogit

---
+++ nvim/.config/nvim/lua/plugins/extras/editor/neogit.lua
@@ -0,0 +1,22 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",  -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  opts = {
+    integrations = {
+      -- If enabled, use telescope for menu selection rather than vim.ui.select.
+      -- Allows multi-select and some things that vim.ui.select doesn't.
+      telescope = true,
+      -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
+      -- The diffview integration enables the diff popup.
+      --
+      -- Requires you to have `sindrets/diffview.nvim` installed.
+      diffview = true,
+    },
+  }
+}
