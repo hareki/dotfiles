2170ca16f527160cc750e5b232f52208f0802f30
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-25 21:27:30 +0700

    updated via script at 25/08/2024 | Sun 09:27 PM

--- nvim/.config/nvim/lua/plugins/extras/coding/yanky.nvim.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/yanky.nvim.lua
@@ -19,8 +19,13 @@ return {
   keys = {
     { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({}) end, desc = "Open Yank History" },
     { "y",         "<Plug>(YankyYank)",                                                          mode = { "n", "x" },                                desc = "Yank text" },
+
     { "p",         "<Plug>(YankyPutAfter)",                                                      mode = { "n" },                                     desc = "Put yanked text after cursor" },
     { "p",         '"_d<Plug>(YankyPutBefore)',                                                  mode = { "x" },                                     desc = "Put yanked text after cursor without overwriting register" },
+
+    { "P",         '"0<Plug>(YankyPutBefore)',                                                   mode = { "n" },                                     desc = "Put yanked text after cursor without overwriting register" },
+    { "P",         '"_d"0<Plug>(YankyPutBefore)',                                                mode = { "x" },                                     desc = "Put yanked text after cursor without overwriting register" },
+
     { "<c-p>",     "<Plug>(YankyPreviousEntry)",                                                 desc = "Select previous entry through yank history" },
     { "<c-n>",     "<Plug>(YankyNextEntry)",                                                     desc = "Select next entry through yank history" },
   },
