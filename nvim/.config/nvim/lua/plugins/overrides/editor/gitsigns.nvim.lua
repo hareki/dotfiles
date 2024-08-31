7ceac80258b30341802266964fdd56aa820ece22
Tu Nguyen <minhtu1392000@gmail.com>
2024-08-31 09:04:51 +0700

    updated via script at 31/08/2024 | Sat 09:04 AM

--- nvim/.config/nvim/lua/plugins/overrides/editor/gitsigns.nvim.lua
+++ nvim/.config/nvim/lua/plugins/overrides/editor/gitsigns.nvim.lua
@@ -17,6 +17,23 @@ return {
     opts.preview_config = {
       border = "rounded",
     }
+
+    local orig_on_attach = opts.on_attach
+    opts.on_attach = function(buffer)
+      orig_on_attach(buffer)
+
+      local function gs_map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      local function gs_unmap(mode, l)
+        vim.keymap.del(mode, l, { buffer = buffer })
+      end
+
+      -- By default LazyVim uses preview_hunk_inline
+      gs_unmap("n", "<leader>ghp")
+      gs_map("n", "<leader>ghp", gitsigns.preview_hunk, "Preview hunk")
+    end
   end,
   keys = {
     {
