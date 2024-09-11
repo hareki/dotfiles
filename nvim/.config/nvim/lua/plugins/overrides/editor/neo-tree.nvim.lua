3fb70b6970ff2b1dd11dea74740a59367c278a42
Tu Nguyen <minhtu1392000@gmail.com>
2024-09-11 20:39:31 +0700

    Updated via sync-g at 11/09/2024 | Wed 08:39 PM

--- nvim/.config/nvim/lua/plugins/overrides/editor/neo-tree.nvim.lua
+++ nvim/.config/nvim/lua/plugins/overrides/editor/neo-tree.nvim.lua
@@ -1,14 +1,5 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  dependencies = {
-    {
-      "adelarsq/image_preview.nvim",
-      event = "VeryLazy",
-      config = function()
-        require("image_preview").setup()
-      end,
-    },
-  },
   opts = function(_, opts)
     -- Show relative line numbers for neo-tree
     -- source: https://stackoverflow.com/questions/77927924/add-relative-line-numbers-in-neo-tree-using-lazy-in-neovim
@@ -23,17 +14,6 @@ return {
     })
 
     opts.commands = opts.commands or {}
-    opts.commands = vim.tbl_extend("force", opts.commands, {
-      image_wezterm = function(state)
-        local node = state.tree:get_node()
-        if node.type == "file" then
-          require("image_preview").PreviewImage(node.path)
-        end
-      end,
-    })
-
-    local filesystem_mappings = Util.ensure_nested_table(opts, "filesystem", "window", "mappings")
-    filesystem_mappings["<leader>p"] = "image_wezterm"
 
     opts.window.mappings["P"] = { "toggle_preview", config = { use_float = true } }
 
