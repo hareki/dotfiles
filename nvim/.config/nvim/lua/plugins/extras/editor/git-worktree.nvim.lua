ca5d96e492c6d859e9e57025028f7bf1d02c096e
Tu Nguyen <minhtu1392000@gmail.com>
2024-09-29 15:47:06 +0700

    Updated via sync-g at 29/09/2024 | Sun 03:47 PM

--- nvim/.config/nvim/lua/plugins/extras/editor/git-worktree.nvim.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/git-worktree.nvim.lua
@@ -1,6 +1,20 @@
 return {
   {
     "polarmutex/git-worktree.nvim",
+    -- Waiting for this to be in the next version
+    -- https://github.com/polarmutex/git-worktree.nvim/commit/604ab2dd763776a36d1aad9fd81a3c513c1d4d94
+    version = false,
     dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      require("telescope").load_extension("git_worktree")
+
+      require("which-key").add({
+        { "<leader>gw", group = "Git Worktree" },
+      })
+    end,
+    keys = {
+      { "<leader>gwl", "<cmd>Telescope git_worktree<cr>", desc = "List" },
+      { "<leader>gwc", "<cmd>Telescope create_git_worktree<cr>", desc = "Create" },
+    },
   },
 }
---
+++ nvim/.config/nvim/lua/plugins/extras/ui/image.nvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "leafo/magick",
+  },
+  {
+    "hareki/image.nvim",
+    opts = {
+      tmux_show_only_in_active_window = true,
+    },
+  },
+}
