361b86aaa483e349e7a9baf6d103f60298efcac0
Tu Nguyen <minhtu1392000@gmail.com>
2024-10-06 10:07:25 +0700

    Updated via sync-g at 06/10/2024 | Sun 10:07 AM

--- nvim/.config/nvim/lua/constant/init.lua
+++ nvim/.config/nvim/lua/constant/init.lua
@@ -14,4 +14,10 @@ M.ESLINT_SUPPORTED = {
   "typescriptreact",
 }
 
+M.BRANCH_FORMATS = {
+  TASK_ID_ONLY = "task id only",
+  TASK_ID_AND_NAME = "task id and name",
+  TASK_ID_AND_AUTHOR = "task id and author",
+}
+
 return M
