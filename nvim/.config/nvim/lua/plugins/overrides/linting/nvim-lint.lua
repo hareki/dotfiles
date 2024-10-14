7e5fa74b848c6fa11369df17ecf511a2b7e1512e
Tu Nguyen <minhtu1392000@gmail.com>
2024-10-14 07:44:41 +0700

    Updated via sync-g at 14/10/2024 | Mon 07:44 AM

--- nvim/.config/nvim/lua/plugins/overrides/linting/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/overrides/linting/nvim-lint.lua
@@ -1,10 +1,19 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = function(_, opts)
-    opts.linters_by_ft = opts.linters_by_ft or {}
-    for _, ft in ipairs(Constant.filetype.ESLINT_SUPPORTED) do
-      opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
-      table.insert(opts.linters_by_ft[ft], "eslint_d")
-    end
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "cspell")
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      for _, ft in ipairs(Constant.filetype.ESLINT_SUPPORTED) do
+        opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+        table.insert(opts.linters_by_ft[ft], "eslint_d")
+      end
+    end,
+  },
 }
