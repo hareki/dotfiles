46d50f44450c8c812d5df9a20498cef11e5f19e2
Tu Nguyen <minhtu1392000@gmail.com>
2024-10-14 07:51:46 +0700

    Updated via sync-g at 14/10/2024 | Mon 07:51 AM

--- nvim/.config/nvim/lua/plugins/extras/editor/neogit.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/neogit.lua
@@ -9,7 +9,6 @@ return {
     "nvim-lua/plenary.nvim", -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
 
-    -- Only one of these is needed, not both.
     "nvim-telescope/telescope.nvim", -- optional
   },
   opts = {
