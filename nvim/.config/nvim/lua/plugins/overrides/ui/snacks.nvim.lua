76fc68569fd8a9847b646b686e8119e0923ffe4b
Tu Nguyen <minhtu1392000@gmail.com>
2024-11-12 13:12:19 +0700

    feat(nvim): update snacks.nvim terminal config

--- nvim/.config/nvim/lua/plugins/overrides/ui/snacks.nvim.lua
+++ nvim/.config/nvim/lua/plugins/overrides/ui/snacks.nvim.lua
@@ -2,6 +2,16 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      terminal = {
+        win = {
+          position = "float",
+          border = "rounded",
+          backdrop = 100,
+          size = { width = 0.7, height = 0.7 },
+          title = " Terminal ",
+          title_pos = "center",
+        },
+      },
       notifier = {
         width = { min = 40, max = 40 },
         margin = { top = 1, right = 1, bottom = 0 },
