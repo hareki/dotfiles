e0c2a01361972b682d37633eda2cc4beeb821b13
Tu Nguyen <minhtu1392000@gmail.com>
2024-11-12 08:11:04 +0700

    feat(nvim): add config file for snacks.nvim

---
+++ nvim/.config/nvim/lua/plugins/overrides/ui/snacks.nvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      notifier = {
+        width = { min = 40, max = 40 },
+        margin = { top = 1, right = 1, bottom = 0 },
+      },
+    },
+  },
+}
