719b6ca59574c5c949376414a8e378c845731f15
Tu Nguyen <minhtu1392000@gmail.com>
2024-11-16 21:37:11 +0700

    chore(nvim): change git-worktree.nvim origin

--- nvim/.config/nvim/lua/plugins/extras/editor/git-worktree.nvim.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/git-worktree.nvim.lua
@@ -18,7 +18,7 @@ end
 
 return {
   {
-    "polarmutex/git-worktree.nvim",
+    "hareki/git-worktree.nvim",
     -- Waiting for this to be in the next version
     -- https://github.com/polarmutex/git-worktree.nvim/commit/604ab2dd763776a36d1aad9fd81a3c513c1d4d94
     version = false,
