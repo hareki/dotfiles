c189dbc623ed47a013af98d8479eb96b961899ed
Tu Nguyen <minhtu1392000@gmail.com>
2024-12-10 08:28:34 +0700

    feat(nvim): update which-key.nvim configs

--- nvim/.config/nvim/lua/plugins/overrides/editor/which-key.nvim.lua
+++ nvim/.config/nvim/lua/plugins/overrides/editor/which-key.nvim.lua
@@ -1,14 +1,66 @@
 return {
   {
     "folke/which-key.nvim",
-    opts = {
-      win = {
+    opts = function(_, opts)
+      opts.win = {
         border = "rounded",
-        padding = { 1, 4 }, -- extra window padding [top/bottom, right/left]
-      },
-      defer = function(ctx)
+        padding = { 1, 3 }, -- extra window padding [top/bottom, right/left]
+      }
+      opts.defer = function(ctx)
         return vim.list_contains({ "<C-V>", "V", "v" }, ctx.mode)
-      end,
-    },
+      end
+
+      local hidden_keymaps = {
+        "[(",
+        "[{",
+        "[<",
+        "[%",
+        "[m",
+        "[M",
+
+        "](",
+        "]{",
+        "]<",
+        "]%",
+        "]m",
+        "]M",
+      }
+      local disabled_spec = {}
+
+      for _, keymap in ipairs(hidden_keymaps) do
+        table.insert(disabled_spec, { keymap, hidden = true })
+      end
+
+      local icons = LazyVim.config.icons.diagnostics
+
+      opts.spec = vim.list_extend(disabled_spec, {
+        {
+          "[s",
+          desc = "Misspelled word",
+          icon = "",
+        },
+        {
+          "]s",
+          desc = "Misspelled word",
+          icon = "",
+        },
+        {
+          "[i",
+          desc = "Indent scope start",
+        },
+        {
+          "]i",
+          desc = "Indent scope bottom",
+        },
+        { "[e", icon = { icon = icons.Error, hl = "DiagnosticError" } },
+        { "]e", icon = { icon = icons.Error, hl = "DiagnosticError" } },
+
+        { "[w", icon = { icon = icons.Warn, hl = "DiagnosticWarn" } },
+        { "]w", icon = { icon = icons.Warn, hl = "DiagnosticWarn" } },
+
+        { "[d", icon = { icon = icons.Info, hl = "DiagnosticInfo" } },
+        { "]d", icon = { icon = icons.Info, hl = "DiagnosticInfo" } },
+      })
+    end,
   },
 }
