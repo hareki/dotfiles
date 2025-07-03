e14d872eef106de7342f3b4acadf9679772629be
Tu Nguyen <minhtu1392000@gmail.com>
2025-07-03 07:55:19 +0700

    feat(nvim): add telescope integration to nvim-notify

--- nvim/.config/nvim/lua/plugins/ui/nvim-notify.lua
+++ nvim/.config/nvim/lua/plugins/ui/nvim-notify.lua
@@ -1,46 +1,56 @@
-local function renderer(bufnr, notif, highlights, config)
-    local api = vim.api
-    local base = require("notify.render.base")
-    local icon = notif.icon
-    local message = notif.message
-    local namespace = base.namespace()
-
-    -- Normal body highlight, same as the stock "minimal" renderer
-    api.nvim_buf_set_lines(bufnr, 0, -1, false, message)
-    api.nvim_buf_set_extmark(bufnr, namespace, 0, 0, {
-        hl_group = highlights.icon,
-        end_line = #message - 1,
-        end_col = #message[#message],
-        priority = 50,
-    })
-
-    -- Let the floating-window border itself carry the title
-    local title = notif.title[1] or ""
-    if notif.duplicates then
-        title = string.format("%s (x%d)", title, #notif.duplicates)
-    end
-    title = string.format(" %s %s ", icon, title)
-
-    -- We don't have the win id to set the title yet => temporarly store it in the buffer variable, set it on `on_open`
-    api.nvim_buf_set_var(bufnr, "notify_border_title", title)
-    api.nvim_buf_set_var(bufnr, "notify_border_title_hl", highlights.title)
-end
-
 return {
     "rcarriga/nvim-notify",
     keys = {
         {
-            "<leader>un",
+            "<leader>ud",
             function()
                 require("notify").dismiss({ silent = true, pending = true })
             end,
             desc = "Dismiss All Notifications",
         },
+        {
+            "<leader>un",
+            function()
+                -- Can possibly use `:Telescope noice` as well
+                require("telescope").extensions.notify.notify({
+                    results_title = "",
+                    preview_title = Constant.telescope.PREVIEW_TITLE,
+                })
+            end,
+            desc = "Show Notification History",
+        }
     },
     opts = {
         stages = "static",
-        timeout = 10000,
-        render = renderer,
+        timeout = 4000,
+        render = function(bufnr, notif, highlights, config)
+            local api = vim.api
+            local base = require("notify.render.base")
+            local icon = notif.icon
+            local message = notif.message
+            local namespace = base.namespace()
+
+            -- Normal body highlight, same as the stock "minimal" renderer
+            api.nvim_buf_set_lines(bufnr, 0, -1, false, message)
+            api.nvim_buf_set_extmark(bufnr, namespace, 0, 0, {
+                hl_group = highlights.icon,
+                end_line = #message - 1,
+                end_col = #message[#message],
+                priority = 50,
+            })
+
+            -- Let the floating-window border itself carry the title
+            local title = notif.title[1] or ""
+            if notif.duplicates then
+                title = string.format("%s (x%d)", title, #notif.duplicates)
+            end
+            title = string.format(" %s %s ", icon, title)
+
+            -- We don't have the win id to set the title yet => temporarly store it in the buffer variable, set it on `on_open`
+            api.nvim_buf_set_var(bufnr, "notify_border_title", title)
+            api.nvim_buf_set_var(bufnr, "notify_border_title_hl", highlights.title)
+        end,
+
         max_height = function()
             return math.floor(vim.o.lines * 0.75)
         end,
