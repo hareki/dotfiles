58eded76854718f78dbe25cebd7d75bc17b2bb05
Tu Nguyen <minhtu1392000@gmail.com>
2025-08-21 07:28:13 +0700

    feat(nvim): add basic diffview.nvim config

--- nvim/.config/nvim/lua/plugins/editor/diffview.nvim.lua
+++ nvim/.config/nvim/lua/plugins/editor/diffview.nvim.lua
@@ -1,1 +1,29 @@
-return {}
+return {
+  {
+    'sindrets/diffview.nvim',
+    cmd = 'DiffviewOpen',
+    keys = {
+      {
+        '<leader>gdl',
+        function()
+          local git = require('utils.git')
+          local current_commit = git.get_current_line_commit()
+
+          if not current_commit then
+            notifier.warn('No commit history for this line')
+            return
+          end
+
+          git.diff_parent(current_commit)
+        end,
+        desc = "current line's commit",
+      },
+      {
+        '<leader>gdt',
+        function()
+          require('utils.git').diff_parent()
+        end,
+      },
+    },
+  },
+}
