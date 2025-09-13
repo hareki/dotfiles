554c759e4bd60051b01c27d790998100b3d21acb
Tu Nguyen <minhtu1392000@gmail.com>
2025-09-13 11:36:48 +0700

    fix(nvim): avoid overriding nvim-tree-preview.lua opts

--- nvim/.config/nvim/lua/plugins/editor/nvim-tree/init.lua
+++ nvim/.config/nvim/lua/plugins/editor/nvim-tree/init.lua
@@ -22,8 +22,19 @@ return {
   end),
   {
     'hareki/nvim-tree-preview.lua',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'herisetiawan00/image.nvim',
+    },
     opts = function()
       return {
+        -- title_format = ' %s ', -- File name
+        title_format = require('configs.common').preview_title.others,
+        zindex = 50, -- The defaul value makes vim.ui.input behind the preview window
+        image_preview = {
+          enable = true,
+        },
+
         on_close = function()
           tree.toggle_tree_height('expand')
         end,
