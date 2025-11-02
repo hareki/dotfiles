40d2410f8c2f633445dbe09bae84ebc6ca8178aa
Tu Nguyen <minhtu1392000@gmail.com>
2025-11-02 15:10:37 +0700

    feat(nvim): add back HlAtCursor user cmd for more granular control

--- nvim/.config/nvim/lua/configs/usercmds.lua
+++ nvim/.config/nvim/lua/configs/usercmds.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_create_user_command('HlAtCursor', function()
+  require('utils.hl-at-cursor')()
+end, { desc = 'Show highlight groups under cursor' })
+
 vim.api.nvim_create_user_command('TabRename', function(command_opts)
   vim.t.tab_name = command_opts.args
   require('lualine').refresh({ place = { 'statusline' } })
