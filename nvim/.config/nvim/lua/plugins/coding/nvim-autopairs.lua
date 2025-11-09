7a6dc7e02f4bc6431b030bc2e29e871ab2f8da41
Tu Nguyen <minhtu1392000@gmail.com>
2025-11-09 10:24:26 +0700

    fix(nvim): disable nvim-autopairs in snacks picker inputs

--- nvim/.config/nvim/lua/plugins/coding/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/coding/nvim-autopairs.lua
@@ -2,6 +2,6 @@ return {
   'windwp/nvim-autopairs',
   opts = {
     fast_wrap = {},
-    disable_filetype = { 'TelescopePrompt', 'vim' },
+    disable_filetype = { 'TelescopePrompt', 'vim', 'snacks_picker_input' },
   },
 }
