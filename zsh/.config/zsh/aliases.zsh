9e1c699dcc8f1a42dcc1c14253a126fe6a0c98f3
Tu Nguyen <minhtu1392000@gmail.com>
2025-11-14 14:10:27 +0700

    fix(zsh): preserve brew cache to save upgrade time espesically dotnet@8

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias lg="lazygit"
 alias ff="fastfetch"
-alias yay='brew update && brew upgrade && brew autoremove && brew cleanup && npm update -g'
+alias yay='brew update && brew upgrade && brew autoremove && npm update -g'
 
 alias cd="z"
 alias cdi="zi"
