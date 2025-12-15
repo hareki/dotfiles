b4510f3ce65cee1a0ba4b8f3a89e7787a0340151
Tu Nguyen <minhtu1392000@gmail.com>
2025-12-15 21:17:04 +0700

    enhance(nvim): enable dropbar for more filetypes to get the file path

--- nvim/.config/nvim/lua/plugins/ui/dropbar/utils.lua
+++ nvim/.config/nvim/lua/plugins/ui/dropbar/utils.lua
@@ -1,16 +1,15 @@
 local M = {}
 
--- Copy of:
--- https://github.com/Bekaboo/dropbar.nvim/blob/a0ad7e24e9dfde6f0b11f6a436f56aeceaf58e9b/lua/dropbar/configs.lua#L265
--- And disable terminal buffers
 ---@type boolean|fun(buf: integer, win: integer, info: table?): boolean
 function M.enable(buf, win, _)
   buf = vim._resolve_bufnr(buf)
-  if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
-    return false
-  end
-
-  if vim.fn.win_gettype(win) ~= '' or vim.wo[win].winbar ~= '' or vim.bo[buf].ft == 'help' then
+  if
+    not vim.api.nvim_buf_is_valid(buf)
+    or not vim.api.nvim_win_is_valid(win)
+    or vim.fn.win_gettype(win) ~= ''
+    or vim.wo[win].winbar ~= ''
+    or vim.bo[buf].ft == 'help'
+  then
     return false
   end
 
@@ -19,12 +18,7 @@ function M.enable(buf, win, _)
     return false
   end
 
-  return vim.bo[buf].ft == 'markdown'
-    or pcall(vim.treesitter.get_parser, buf)
-    or not vim.tbl_isempty(vim.lsp.get_clients({
-      bufnr = buf,
-      method = vim.lsp.protocol.Methods.textDocument_documentSymbol,
-    }))
+  return true
 end
 
 return M
