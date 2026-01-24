e24b747edabf6a193f534ab9aaa6bc33597a062b
Tu Nguyen <minhtu1392000@gmail.com>
2026-01-24 11:13:41 +0700

    refactor(nvim): improve harpoon file management and key mapping logic

--- nvim/.config/nvim/lua/plugins/editor/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/editor/harpoon.lua
@@ -13,74 +13,95 @@ return {
       },
     }
 
-    for i = 1, 5 do
+    for current_index = 1, 5 do
       table.insert(keys, {
-        '<leader>H' .. i,
+        '<leader>H' .. current_index,
         function()
           local harpoon = require('harpoon')
           local path = require('utils.path')
           local list = harpoon:list()
-          local item = list.config.create_list_item(list.config)
 
-          local filepath = vim.api.nvim_buf_get_name(0)
-          local relpath = path.get_relative_path(filepath, vim.uv.cwd() or vim.fn.getcwd())
+          local new_item = list.config.create_list_item(list.config)
+          local old_item = list:get(current_index)
 
-          local existing_item = list:get(i)
-          local old_filepath = existing_item and existing_item.value or nil
+          local new_filepath = vim.api.nvim_buf_get_name(0)
+          local new_relpath = path.get_relative_path(new_filepath, vim.uv.cwd() or vim.fn.getcwd())
+
+          local old_filepath = old_item and old_item.value or nil
           local old_relpath = old_filepath
               and path.get_relative_path(old_filepath, vim.uv.cwd() or vim.fn.getcwd())
             or nil
 
-          if existing_item and existing_item.value == item.value then
+          if old_item and old_item.value == new_item.value then
             Notifier.warn({
-              { relpath, 'NotifyWARNTitle' },
+              { new_relpath, 'NotifyWARNTitle' },
               { '\nis already in slot ', 'Normal' },
-              { tostring(i), 'NotifyWARNTitle' },
+              { tostring(current_index), 'NotifyWARNTitle' },
             }, { title = 'harpoon' })
             return
           end
 
-          local _, existing_index = list:get_by_value(item.value)
-          if existing_index and existing_index ~= i then
-            list:remove_at(existing_index)
+          -- Check if new_item already exists at another index
+          local old_index = nil
+          local length = list:length()
+          for index = 1, length do
+            if index ~= current_index then
+              local existing_item = list:get(index)
+              if existing_item and existing_item.value == new_item.value then
+                old_index = index
+                break
+              end
+            end
           end
 
-          if existing_item then
-            list:remove_at(i)
-          end
+          -- Replace the item at slot i with new_item. If new_item exists in another slot, it is removed from that slot first.
+          list:replace_at(current_index, new_item)
 
-          list.items[i] = item
+          local item_exists_elsewhere = old_index ~= nil
+          local slot_has_different_item = old_relpath ~= nil and old_relpath ~= new_relpath
+          local hl_title = (item_exists_elsewhere or slot_has_different_item) and 'NotifyWARNTitle'
+            or 'NotifyINFOTitle'
 
-          if old_relpath then
+          if item_exists_elsewhere then
+            Notifier.warn({
+              { 'Moved ', 'Normal' },
+              { new_relpath, hl_title },
+              { '\nfrom slot ', 'Normal' },
+              { tostring(old_index), hl_title },
+              { ' to ', 'Normal' },
+              { tostring(current_index), hl_title },
+            }, { title = 'harpoon' })
+          elseif slot_has_different_item then
             Notifier.warn({
               { 'Replaced ', 'Normal' },
-              { old_relpath, 'NotifyWARNTitle' },
+              { old_relpath, hl_title },
               { '\nwith ', 'Normal' },
-              { relpath, 'NotifyWARNTitle' },
+              { new_relpath, hl_title },
               { '\nfor slot ', 'Normal' },
-              { tostring(i), 'NotifyWARNTitle' },
+              { tostring(current_index), hl_title },
             }, { title = 'harpoon' })
           else
+            -- Slot was empty, new file added
             Notifier.info({
               { 'Added ', 'Normal' },
-              { relpath, 'NotifyINFOTitle' },
+              { new_relpath, hl_title },
               { '\ninto slot ', 'Normal' },
-              { tostring(i), 'NotifyINFOTitle' },
+              { tostring(current_index), hl_title },
             }, { title = 'harpoon' })
           end
 
           local lualine_utils = require('plugins.ui.lualine.utils')
           lualine_utils.refresh_statusline()
         end,
-        desc = 'Harpoon Current File to Slot ' .. i,
+        desc = 'Harpoon Current File to Slot ' .. current_index,
       })
 
       table.insert(keys, {
-        '<leader>' .. i,
+        '<leader>' .. current_index,
         function()
-          require('harpoon'):list():select(i)
+          require('harpoon'):list():select(current_index)
         end,
-        desc = 'Harpoon: File ' .. i,
+        desc = 'Harpoon: File ' .. current_index,
       })
     end
 
