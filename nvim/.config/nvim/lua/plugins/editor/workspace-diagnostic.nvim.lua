0460fdc44c5aa4bc67133321cec1d16e8648762b
Tu Nguyen <minhtu1392000@gmail.com>
2026-01-27 22:04:54 +0700

    feat(nvim--workspace-diagnostic): populate workspace diagnostic when attaching lsp client

--- nvim/.config/nvim/lua/plugins/editor/workspace-diagnostic.nvim.lua
+++ nvim/.config/nvim/lua/plugins/editor/workspace-diagnostic.nvim.lua
@@ -1,26 +1,4 @@
 return {
   'artemave/workspace-diagnostics.nvim',
-  keys = {
-    {
-      '<leader>x',
-      function()
-        local clients = vim.lsp.get_clients({ bufnr = 0 })
-        local lines = {}
-        for idx, client in ipairs(clients) do
-          lines[#lines + 1] = string.format('  %d. %s', idx, client.name)
-        end
-
-        local body = #lines > 0 and table.concat(lines, '\n') or 'No clients'
-
-        Notifier.info('Fetching workspace diagnostics from:\n' .. body)
-        local workspace_diagnostics = require('workspace-diagnostics')
-        for _, client in ipairs(clients) do
-          workspace_diagnostics.populate_workspace_diagnostics(client, 0)
-        end
-      end,
-
-      desc = 'Fetch Workspace Diagnostics',
-    },
-  },
   opts = {},
 }
