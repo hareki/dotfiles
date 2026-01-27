d889ebb076245e138132d7fbfa24578c34e75a28
Tu Nguyen <minhtu1392000@gmail.com>
2026-01-27 22:06:52 +0700

    docs(nvim--workspace-diagnostic): add TODO comment

--- nvim/.config/nvim/lua/plugins/editor/workspace-diagnostic.nvim.lua
+++ nvim/.config/nvim/lua/plugins/editor/workspace-diagnostic.nvim.lua
@@ -1,3 +1,5 @@
+-- TODO: Remove this plugin after this issue is resolved
+-- https://github.com/artemave/workspace-diagnostics.nvim/issues/20
 return {
   'artemave/workspace-diagnostics.nvim',
   opts = {},
