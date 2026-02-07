a02c8cbcdcaa1622ef2152d0056182a050ea5798
Tu Nguyen <minhtu1392000@gmail.com>
2026-02-07 19:11:04 +0700

    feat(nvim/config/color): add primitive layer

--- nvim/.config/nvim/lua/config/color.lua
+++ nvim/.config/nvim/lua/config/color.lua
@@ -1,15 +1,26 @@
 ---@class config.color
 local M = {}
 
-M.sub_cursor_bg = '#414e70' -- Lightened blue
-M.visual_bg = '#4f5164' -- Darkened surface2
+local original = require('utils.ui').get_palette()
+local extension = {
+  blue0 = '#323c56', -- Darkest blue
+  blue1 = '#414e70', -- Mid blue
+  blue2 = '#495d83', -- Brightest blue
 
--- Taken from VSCode Catppuccin Theme
-M.git_conflict_current_label_bg = '#57735b'
-M.git_conflict_current_bg = '#394841'
-M.git_conflict_ancestor_label_bg = '#585b70'
-M.git_conflict_ancestor_bg = '#44465a'
-M.git_conflict_incoming_label_bg = '#495d83'
-M.git_conflict_incoming_bg = '#323c56'
+  green0 = '#394841', -- Darker green
+  green1 = '#57735b', -- Brighter green
+
+  surface15 = '#4f5164', -- Between palette surface1 and surface2
+}
+
+M.sub_cursor_bg = extension.blue1
+M.visual_bg = extension.surface15
+
+M.git_conflict_current_label_bg = extension.green1
+M.git_conflict_current_bg = extension.green0
+M.git_conflict_ancestor_label_bg = original.surface2
+M.git_conflict_ancestor_bg = original.surface1
+M.git_conflict_incoming_label_bg = extension.blue2
+M.git_conflict_incoming_bg = extension.blue0
 
 return M
