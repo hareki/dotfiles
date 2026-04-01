766bbb868842777a805fb2c516de326ae563aba7
Tu Nguyen <minhtu1392000@gmail.com>
2026-04-01 08:26:17 +0700

    feat(tmuxinator): add tmuxinator as a private git submodule

--- zsh/.config/zsh/compdef.zsh
+++ zsh/.config/zsh/compdef.zsh
@@ -23,3 +23,26 @@ _tv_autocomplete() {
 }
 
 compdef _tv_autocomplete tv
+
+# https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh
+_tmuxinator() {
+  local commands projects
+  commands=(${(f)"$(tmuxinator commands zsh)"})
+  projects=(${(f)"$(tmuxinator completions start)"})
+
+  if (( CURRENT == 2 )); then
+    _alternative \
+      'commands:: _describe -t commands "tmuxinator subcommands" commands' \
+      'projects:: _describe -t projects "tmuxinator projects" projects'
+  elif (( CURRENT == 3)); then
+    case $words[2] in
+      copy|cp|c|debug|delete|rm|open|o|start|s|stop|edit|e)
+        _arguments '*:projects:($projects)'
+      ;;
+    esac
+  fi
+
+  return
+}
+
+compdef _tmuxinator tmuxinator
