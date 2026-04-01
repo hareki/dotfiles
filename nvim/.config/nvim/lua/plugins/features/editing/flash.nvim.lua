14cd436f2b932751333d0a79e3de933938bb1268
Tu Nguyen <minhtu1392000@gmail.com>
2026-04-01 20:56:09 +0700

    feat(nvim/flash.nvim): add plugin

---
+++ nvim/.config/nvim/lua/plugins/features/editing/flash.nvim.lua
@@ -0,0 +1,55 @@
+return {
+  'folke/flash.nvim',
+  ---@type Flash.Config
+  opts = {},
+
+  keys = {
+    {
+      's',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    -- { '<C-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search', },
+    -- Simulate nvim-treesitter incremental selection
+    {
+      '<C-Space>',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter({
+          actions = {
+            ['<C-Space>'] = 'next',
+            ['<BS>'] = 'prev',
+          },
+        })
+      end,
+      desc = 'Treesitter Incremental Selection',
+    },
+  },
+}
