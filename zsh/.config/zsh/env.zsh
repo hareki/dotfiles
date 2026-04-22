de621b18b7fbe3037d3ebe9ec34d366d9cc4369f
Tu Nguyen <minhtu1392000@gmail.com>
2026-04-22 13:10:13 +0700

    style(zsh/fzf): remove scrollbar

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -22,7 +22,7 @@ export _ZO_FZF_OPTS="$_ZO_FZF_OPTS $FZF_DEFAULT_OPTS"
 # FZF Catppuccin Mocha color
 export FZF_DEFAULT_OPTS=" \
 --border=rounded --layout=reverse --cycle --info=inline-right --info-command='echo \"\${FZF_MATCH_COUNT}/\${FZF_TOTAL_COUNT}\"' --pointer='' --highlight-line \
---preview-window noinfo --scrollbar '▌▐' --prompt='  ' \
+--preview-window noinfo --scrollbar '' --prompt='  ' \
 --color=bg+:#313244,gutter:#1e1e2e,bg:#1e1e2e,spinner:#f5e0dc,hl:#89b4fa,border:#89b4fa \
 --color=fg:#cdd6f4,header:#f38ba8,info:#6c7086,pointer:#f5e0dc,label:#89b4fa::bold \
 --color=marker:#89b4fa,fg+:#cdd6f4:regular,prompt:#89b4fa,hl+:#89b4fa:regular,query::regular \
