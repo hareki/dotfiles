8f36a5627c9e9bb7d18b2050d1e2bcb25ab6dfde
Tu Nguyen <minhtu1392000@gmail.com>
2026-05-25 13:28:09 +0700

    refactor(nvim): try using oxlint with conform.nvim instead

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -53,7 +53,6 @@ map({ 'n', 'i' }, '<A-s>', function()
 end, { desc = 'Format and Save' })
 
 map({ 'n', 'i' }, '<leader>F', function()
-  -- Still partially save the file when linter is oxlint, see `lua/utils/linters/oxlint.lua`
   style_enforcer().run({
     save = false,
   })
--- nvim/.config/nvim/lua/plugins/features/formatting/conform.nvim.lua
+++ nvim/.config/nvim/lua/plugins/features/formatting/conform.nvim.lua
@@ -3,11 +3,21 @@ return {
   opts = function()
     local js_formatter_name = Project.formatter == 'oxfmt' and 'oxfmt' or 'prettier'
 
+    -- JS-family filetypes run the chosen formatter, then oxlint --fix when the
+    -- project linter is oxlint (replaces the DIY oxc.fixAll LSP runner).
+    local js_all_config = { js_formatter_name, stop_after_first = true }
+    if Project.linter == 'oxlint' then
+      js_all_config = { js_formatter_name, 'oxlint' }
+    end
+
     local formatter_groups = {
+      js_all = {
+        config = js_all_config,
+        filetypes = Filetypes.js_all,
+      },
       [js_formatter_name] = {
         config = { js_formatter_name, stop_after_first = true },
         filetypes = Filetypes.merge(
-          Filetypes.js_all,
           Filetypes.css,
           { 'html' },
           Filetypes.markdown,
