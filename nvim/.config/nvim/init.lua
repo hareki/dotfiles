40f52f0fb442a1a52bfe86b5ac4051a2e56f8ffe
Tu Nguyen <minhtu1392000@gmail.com>
2026-06-10 11:46:29 +0700

    docs(nvim): update demo.png

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,3 @@
--- Use Neovim's experimental Lua module loader that does byte-caching of Lua modules
--- Taken from https://github.com/mrjones2014/dotfiles/blob/master/nvim/init.lua#L2
 vim.loader.enable(true)
 
 require('config.globals')
