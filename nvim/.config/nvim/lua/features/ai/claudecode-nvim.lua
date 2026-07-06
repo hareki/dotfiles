3ccef0fb89f33711c93c13e1eda949000dc7e132
Tu Nguyen <minhtu1392000@gmail.com>
2026-07-06 21:02:07 +0700

    impr(nvim/claudecode.nvim): update terminal window title label

--- nvim/.config/nvim/lua/features/ai/claudecode-nvim.lua
+++ nvim/.config/nvim/lua/features/ai/claudecode-nvim.lua
@@ -77,7 +77,7 @@ return {
           --- @module "snacks"
           --- @type snacks.win.Config | {}
           snacks_win_opts = {
-            title = ' Agentic Coding ',
+            title = ' Coding Agent ',
             position = 'float',
             width = size.width,
             height = size.height,
