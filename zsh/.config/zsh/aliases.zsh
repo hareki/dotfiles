894bae5d7b81cb79b7fe620ba57fbe90d0ff79d4
Tu Nguyen <minhtu1392000@gmail.com>
2026-07-07 12:17:05 +0700

    feat(zsh): update aliases.zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,12 +1,15 @@
-alias lg="lazygit"
-
-alias ..="z .."
-alias ...="z ..."
-alias ....="z ...."
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
 
 alias compz="zcompile ~/.zshrc"
 alias cl="clear"
+alias cc="claude"
+alias nv="nvim"
+alias lg="lazygit"
+
 alias ez="nvim ~/.zshrc"
 alias eze="nvim ~/.zshenv"
+
 alias tree="eza --tree"
 alias submodule-update="git submodule update --init --recursive --remote"
